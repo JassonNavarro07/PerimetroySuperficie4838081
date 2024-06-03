a2f5ddd2054d58641746b061f62152356b2e0522
Jasson <Jasson@DESKTOP-S5KM2ME>
2024-06-02 23:22:44 -0500

    Agregar archivos de proyecto.

---
+++ PerimetroySuperficie4838081/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PerimetroySuperficie4838081
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ PerimetroySuperficie4838081/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace PerimetroySuperficie4838081
+
+{
+
+
+
+    public partial class MainPage : ContentPage
+    {
+
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        #region evento button
+        /// <summary>
+        /// evento de button que acciona el calculo
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //declaracion de variables
+            double altura,basee,perimetro,superficie;
+            altura = Convert.ToDouble(entryA.Text);
+            basee = Convert.ToDouble(entryB.Text);
+            perimetro = 2 * (basee + altura);
+            superficie = basee * altura;
+            //mostrando resultados
+            entryP.Text = perimetro.ToString();
+            entryS.Text = superficie.ToString();
+        }
+        #endregion
+    }
+
+}
